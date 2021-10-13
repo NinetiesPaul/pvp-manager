44ead801c71eae791643081f053a34d1f79307bf
Paul Richard Pereire Martins dos Anjos <paulrpm@gmail.com>
2021-10-13 13:27:59 -0300

    Added cross-team vulnerability icon

--- js/pvp_assembler.js
+++ js/pvp_assembler.js
@@ -1,5 +1,6 @@
 $(document).on('click', '.pkm-list-btn', function() {
 	$(".teamassembler-table tbody").html("");
+	$(".pkm-list-btn").attr("disabled", true)
 
 	var pkms = $(".pkm-list").val();
 	pkms = pkms.split(',');
@@ -21,21 +22,30 @@ $(document).on('click', '.pkm-list-btn', function() {
 
 	teams = [...new Set(teams)];
 
-	finalTeams = [];
+	var textToAppend = '';
 
 	$.each(teams, function(mk,v) {
 		pkms = v.split(",");
 		var slot1 = pkms[0],
 			slot2 = pkms[1],
 			slot3 = pkms[2],
-			resistances = 0,vulnerabilities = 0;
+			resistances = 0,vulnerabilities = 0, itVulnerability = false, combinedVulnerabilites = [];
 		$.each(pkms, function(k,v) {
 			resistances += Object.keys(pokeDB[v].defense_data.resistant_to).length;
 			vulnerabilities += Object.keys(pokeDB[v].defense_data.vulnerable_to).length;
+			vulnerableToTypes = $.map(pokeDB[v].defense_data.vulnerable_to, function(element,index) {return index});
+			$.each(vulnerableToTypes, function(k,v) {
+				combinedVulnerabilites.push(v);
+			})
 		});
-		$("#assembler-tbody").append(
-			"<tr><td><b>"+slot1+"</b><br><small>"+pokeDB[slot1].type.join("/")+"</small></td><td><b>"+slot2+"</b><br><small>"+pokeDB[slot2].type.join("/")+"</small></td><td><b>"+slot3+"</b><br><small>"+pokeDB[slot3].type.join("/")+"</small></td><td>"+resistances+"</td><td>"+vulnerabilities+"</td></tr>"
-		);
+
+		ctVulnerability = ((new Set(combinedVulnerabilites).size) !== combinedVulnerabilites.length) ? "glyphicon glyphicon-thumbs-down" : "glyphicon glyphicon-thumbs-up";
+		//console.log(itVulnerability)
+
+		textToAppend += "<tr><td><b>"+slot1+"</b><br><small>"+pokeDB[slot1].type.join("/")+"</small></td><td><b>"+slot2+"</b><br><small>"+pokeDB[slot2].type.join("/")+"</small></td><td><b>"+slot3+"</b><br><small>"+pokeDB[slot3].type.join("/")+"</small></td><td>"+resistances+"</td><td>"+vulnerabilities+"</td><td><span class=\""+ctVulnerability+"\" aria-hidden=\"true\"></span></tr>";
 	});
+
+	$("#assembler-tbody").append(textToAppend);
+	$(".pkm-list-btn").attr("disabled", false);
 
 });
