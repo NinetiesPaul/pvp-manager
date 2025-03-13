23d0575b4b344383bfd134a4fa72caea5f08729d
Paul Richard <paulrpm@gmail.com>
2025-03-13 09:01:55 -0300

    feat: created method dedicated to convert slot data into object, for loading and saving

--- js/builder_save.js
+++ js/builder_save.js
@@ -7,83 +7,20 @@ function saveTeam(switchPositions = false, target = null)
 	dataToSave = ''
 
 	if (pkmSlot1 !== "-- Pick an option --") {
-		pkmSlot1 = pkmSlot1.split(" - ")
-		pkmSlot1 = [1, pokeDB[pkmSlot1[1]].name]
-
-		quick = $("#quick_move-slot1").find(":selected").text()
-		if (quick !== "-- Quick --") {
-			quick = quick.split(" (")[0];
-			pkmSlot1.push(quick)
-		}
-
-		charge1 = $("#charge1_move-slot1").find(":selected").text()
-		if (charge1 !== "-- Charge I --") {
-			charge1 = charge1.split(" (")[0];
-			pkmSlot1.push(charge1)
-		}
-
-		charge2 = $("#charge2_move-slot1").find(":selected").text()
-		if (charge2 !== "-- Charge II --") {
-			charge2 = charge2.split(" (")[0];
-			pkmSlot1.push(charge2)
-		}
-
-		pkmSlot1 = pkmSlot1.join(",")
-
-		dataToSave = (dataToSave == '') ? dataToSave += pkmSlot1 : dataToSave += "&" + pkmSlot1
+		pkmSlot1 = slotToObject(pkmSlot1, 1);
+		pkmSlot1 = pkmSlot1.join(",");
+		dataToSave = (dataToSave == '') ? dataToSave += pkmSlot1 : dataToSave += "&" + pkmSlot1;
 	}
 
 	if (pkmSlot2 !== "-- Pick an option --") {
-		pkmSlot2 = pkmSlot2.split(" - ")
-		pkmSlot2 = [2, pokeDB[pkmSlot2[1]].name]
-
-		quick = $("#quick_move-slot2").find(":selected").text()
-		if (quick !== "-- Quick --") {
-			quick = quick.split(" (")[0];
-			pkmSlot2.push(quick)
-		}
-
-		charge1 = $("#charge1_move-slot2").find(":selected").text()
-		if (charge1 !== "-- Charge I --") {
-			charge1 = charge1.split(" (")[0];
-			pkmSlot2.push(charge1)
-		}
-
-		charge2 = $("#charge2_move-slot2").find(":selected").text()
-		if (charge2 !== "-- Charge II --") {
-			charge2 = charge2.split(" (")[0];
-			pkmSlot2.push(charge2)
-		}
-
-		pkmSlot2 = pkmSlot2.join(",")
-
+		pkmSlot2 = slotToObject(pkmSlot2, 2);
+		pkmSlot2 = pkmSlot2.join(",");
 		dataToSave = (dataToSave == '') ? dataToSave += pkmSlot2 : dataToSave += "&" + pkmSlot2
 	}
 
 	if (pkmSlot3 !== "-- Pick an option --") {
-		pkmSlot3 = pkmSlot3.split(" - ")
-		pkmSlot3 = [3, pokeDB[pkmSlot3[1]].name]
-		
-		quick = $("#quick_move-slot3").find(":selected").text()
-		if (quick !== "-- Quick --") {
-			quick = quick.split(" (")[0];
-			pkmSlot3.push(quick)
-		}
-
-		charge1 = $("#charge1_move-slot3").find(":selected").text()
-		if (charge1 !== "-- Charge I --") {
-			charge1 = charge1.split(" (")[0];
-			pkmSlot3.push(charge1)
-		}
-
-		charge2 = $("#charge2_move-slot3").find(":selected").text()
-		if (charge2 !== "-- Charge II --") {
-			charge2 = charge2.split(" (")[0];
-			pkmSlot3.push(charge2)
-		}
-
-		pkmSlot3 = pkmSlot3.join(",")
-
+		pkmSlot3 = slotToObject(pkmSlot3, 3);
+		pkmSlot3 = pkmSlot3.join(",");
 		dataToSave = (dataToSave == '') ? dataToSave += pkmSlot3 : dataToSave += "&" + pkmSlot3
 	}
 
@@ -99,4 +36,30 @@ function saveTeam(switchPositions = false, target = null)
 		$("#saveData").val(dataToSave)
 		new ClipboardJS('.saveBtn');
 	}
+}
+
+function slotToObject(pkmSlot, slot)
+{
+	pkmSlot = pkmSlot.split(" - ")
+	pkmSlot = [slot, pokeDB[pkmSlot[1]].name]
+
+	quick = $("#quick_move-slot" + slot).find(":selected").text()
+	if (quick !== "-- Quick --") {
+		quick = quick.split(" (")[0];
+		pkmSlot.push(quick)
+	}
+
+	charge1 = $("#charge1_move-slot" + slot).find(":selected").text()
+	if (charge1 !== "-- Charge I --") {
+		charge1 = charge1.split(" (")[0];
+		pkmSlot.push(charge1)
+	}
+
+	charge2 = $("#charge2_move-slot" + slot).find(":selected").text()
+	if (charge2 !== "-- Charge II --") {
+		charge2 = charge2.split(" (")[0];
+		pkmSlot.push(charge2)
+	}
+
+	return pkmSlot;
 }
